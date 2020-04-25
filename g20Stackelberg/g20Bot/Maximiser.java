c6f69c28b94bdc71ad962a485ea65473c8392523
mbaxask2 <sarah.kells@student.manchester.ac.uk>
2020-04-25 15:11:49 +0100

    Adding Quadratic Maximiser

---
+++ g20Stackelberg/g20Bot/Maximiser.java
@@ -0,0 +1,9 @@
+package g20Bot;
+
+public interface Maximiser {
+
+    public float getProfit(int day, float lPrice, float fPrice);
+
+    public float getBestPrice(Regression rModel, int day);
+
+}

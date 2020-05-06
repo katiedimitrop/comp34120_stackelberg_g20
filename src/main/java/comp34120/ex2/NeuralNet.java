599d6132bbc49478e241bb070f9328a89b110d61
Phoebe Harris <phoebe.harris-2@student.manchester.ac.uk>
2020-05-06 19:31:54 +0100

    cleaned up code

--- src/main/java/comp34120/ex2/NeuralNet.java
+++ src/main/java/comp34120/ex2/NeuralNet.java
@@ -46,7 +46,7 @@ public class NeuralNet {
     private INDArray constFeature;
     int rngSeed = 123; // random number seed for reproducibility
     private static final int nEpochs = 500;
-    private static final double learningRate = 0.00001;
+    private static final double learningRate = 0.00002;
     private static final int numInput = 1;
     private static final int numOutputs = 1;
     private static final int nHidden = 1;
@@ -86,53 +86,10 @@ public class NeuralNet {
 
          DataSet allData = new DataSet(leaderPriceFeature, followerPriceFeature);
 
-         //create entire dataset
-        // Partitions the data transform by the specified number.
-        // public List<DataSet> dataSetBatches(int num)
-
-
-       // Dataset trainingData = allData.next(80);
-       // DataSet inputData = new DataSet(inputData, followerPriceFeature);
-         //
-        // outputData = new DataSet(followerPriceFeature);
-
-        /* CSV, diff
-
-        //First: get the dataset using the record reader. CSVRecordReader handles loading/parsing
-         int numLinesToSkip = 0;
-         String delimiter = ",";
-         RecordReader rr= new CSVRecordReader(numLinesToSkip, delimiter);
-         rr.initialize(new FileSplit(new File("../Mk1_train.csv")));
-         //Pass record reader to iterator to build training dataset
-         //label index is 2
-        //Record reader dataset iterator. Takes a DataVec RecordReader as input, and handles the conversion to ND4J
-        // DataSet objects as well as producing minibatches from individual records.
-
-        //DataSetIterator trainIter = new RecordReaderDataSetIterator(rr,batchSize,2,2,true);
-
-
-         //Load the test/evaluation data:
-         //RecordReader rrTest = new CSVRecordReader();
-         //rrTest.initialize(new FileSplit(new File("../Mk1_test.csv")));
-         //DataSetIterator testIter = new RecordReaderDataSetIterator(rrTest,batchSize,2,2,true);
-*/
-/*
-        OutputLayer outputLayer = new OutputLayer.Builder().nIn(nHidden)
-                .nOut(numOutputs)
-                .lossFunction(LossFunctions.LossFunction.MSE)
-                //.activation(Activation.IDENTITY)
-                .build();
-
-        DenseLayer linearLayer = new DenseLayer.Builder().nIn(numInput)
-                .nOut(nHidden)
-                .activation(Activation.IDENTITY)
-                .build();
-*/
+      
         this.neuralNetwork = new MultiLayerNetwork(new NeuralNetConfiguration.Builder()
                         .seed(rngSeed)
-                        //.iterations(iterations)
                         .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                        //.learningRate(learningRate)
                         .weightInit(WeightInit.XAVIER)
                         .updater(new Nesterovs(0.9))
                         .list()
@@ -141,54 +98,18 @@ public class NeuralNet {
                                 .nIn(numInput).nOut(numOutputs).build())
                         .build());
 
-                /*.seed(rngSeed)
-                //.weightInit(WeightInit.XAVIER)
-                .updater(new Adam(learningRate,
-                        Adam.DEFAULT_ADAM_BETA1_MEAN_DECAY,
-                        Adam.DEFAULT_ADAM_BETA2_VAR_DECAY,
-                        Adam.DEFAULT_ADAM_EPSILON))
-                .list()
-                .layer(0, linearLayer)
-                .layer(1, outputLayer)
-                .pretrain(false)
-                .backprop(true)
-                .build());*/
-
         neuralNetwork.init();
 
-//        Debugging listerner, not needed in production
-//        neuralNetwork.setListeners(new ScoreIterationListener(1));
-
         DataSetIterator allDataIterator = new ListDataSetIterator<>(allData.asList());
-        //Normalize the training data
-        //DataNormalization normalizer = new NormalizerStandardize();
-        //normalizer.fit(trainData);              //Collect training data statistics
-        //allDataIterator.reset();
 
-        //Use previously collected statistics to normalize on-the-fly. Each DataSet returned by 'trainData' iterator will be normalized
-        //allDataIterator.setPreProcessor(normalizer);
-
-
-        //System.out.println("Input "+trainIter.inputColumns());
-        //System.out.println("getLabels "+trainIter.getLabels());
-        //Train the network on the full data set, and evaluate in periodically
-        //print the score with every 1 iteration
         neuralNetwork.setListeners(new ScoreIterationListener(1));
         System.out.println("Training neural Network....");
-        //train
+        //Training the NN
         for (int i = 0; i < nEpochs; i++) {
-            //trainIter.reset();
             allDataIterator.reset();
-            //train the neural net
             neuralNetwork.fit(allDataIterator);
-            //neuralNetwork.fit(trainIter);
         }
 
-
-
-        //neuralNetwork.save(new File("...");
-
-        //MultiLayerNetwork net2 = MultiLayerNetwork.load(new File("..."), true);
         Map<String, INDArray> paramTable = neuralNetwork.paramTable();
         Set<String> keys = paramTable.keySet();
         Iterator<String> it = keys.iterator();
@@ -200,11 +121,8 @@ public class NeuralNet {
             System.out.print(key+" ");//print keys
             System.out.println(Arrays.toString(values.shape()));//print shape of INDArray
             System.out.println(values);
-            //neuralNetwork.setParam(key, Nd4j.rand(values.shape()));//set some random values
         }
 
-
-        //System.out.println("Evaluating model....");
         System.out.println("****************Example finished********************");
       }//initialization method
     }//class
